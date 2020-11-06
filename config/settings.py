0f1dd3480dd844530f6f341e17a21d56a8aacedc
Ashish Goyal <a.sh2310goyal@gmail.com>
2020-11-06 16:24:25 +0530

    Updated for heroku deployment using gunicorn

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '#k^ll2tat!ya@_z8wya+a$ig#22p2mz)4!n1*gg0k7z4^!n7+$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['online-examintion.herokuapp.com']
 
 
 # Application definition
